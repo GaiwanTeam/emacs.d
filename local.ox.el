62724fc9a8988fdff45d3559fffbf44d005b09ba
Mitesh Shah <mitesh@miteshshah.com>
2023-11-07 20:29:00 +0530

    👾 ox's config

---
+++ local.ox.el
@@ -0,0 +1,91 @@
+;; Allow Ctrl-u to scroll up a page like vim
+(setq evil-want-C-u-scroll t)
+
+;; You will most likely need to adjust this font size for your system!
+(defvar ox/default-font-size 180)
+(defvar ox/default-variable-font-size 180)
+(set-face-attribute 'default nil :font "Iosevka" :height ox/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Iosevka" :height ox/default-font-size)
+
+;; Unfortunately emacs launched from `.app` launcher does not get the full exec path which our shell has. Let's fix that
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  (fset 'evil-visual-update-x-selection 'ignore)
+  (setq evil-kill-on-visual-paste nil)
+  (setq-default evil-symbol-word-search t)
+  :config
+  (evil-mode t)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (require 'evil-maps)
+  (define-key evil-motion-state-map "L" nil)
+  (define-key evil-motion-state-map "M" nil)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+;; Use another key to go into normal / escape mode. I have it configured as `qp`
+(use-package evil-escape
+  :config
+  (setq-default evil-escape-key-sequence "qp")
+  (evil-escape-mode))
+
+(use-package evil-cleverparens
+  :after (evil smartparens)
+  :commands evil-cleverparens-mode
+  :init
+  (add-hook 'clojure-mode-hook #'evil-cleverparens-mode)
+  (add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
+  (setq evil-cleverparens-complete-parens-in-yanked-region t)
+  :config
+  (setq evil-cleverparens-use-s-and-S nil)
+  (evil-define-key '(normal visual) evil-cleverparens-mode-map
+    "s" nil
+    "S" nil
+    "{" nil
+    "}" nil
+    "[" nil
+    "]" nil
+    (kbd "<tab>") 'evil-jump-item))
+
+(use-package projectile
+  :init
+  (setq projectile-create-missing-test-files t)
+  (setq projectile-project-search-path '("~/projects/" "~/playground/" "~/projects/lambdaisland"))
+  (defun ox/refresh-projects-dir ()
+    (interactive)
+    ;; (projectile-discover-projects-in-directory "~/projects")
+    (projectile-discover-projects-in-search-path))
+  :config
+  (projectile-global-mode))
+
+;; command-log-mode is useful for displaying a panel showing each key binding
+;; you use in a panel on the right side of the frame. Great for live streams and
+;; screencasts!
+(use-package command-log-mode)
+
+(use-package forge)
+
+(use-package git-link
+  :config
+  (setq git-link-open-in-browser t
+        git-link-use-commit t))
+
+(use-package emojify)
+(use-package gitmoji
+  :ensure nil
+  :load-path "~/projects/emacs-gitmoji")
